fbb176875d36afad83a34675b68cb1dec3947cc4
christianhargett <68342183+christianhargett@users.noreply.github.com>
2020-08-02 15:04:02 -0400

    Delete test.py

--- analysis/test.py
+++
@@ -1,9 +0,0 @@
-# Initial variable to track game play
-user_play = "y"
-
-# While we are still playing...
-while user_play == "y":
-    user_numbers = int(input("How many numbers?"))
-    for number in range(user_numbers):
-        print(number)
-    user_play = input("Do you want to continue: (y)es or (n)o")
